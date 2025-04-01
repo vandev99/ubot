d5f14e019a8682af7858236b9f7ccdfaf4c25a75
ricoogh <oioioilwkcloud@gmail.com>
2025-04-01 14:15:24 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -312,9 +312,8 @@ async def _(client, callback_query):
     await bash("rm -rf *session*")
     await install_my_peer(new_client)
     try:
-        await new_client.join_chat("testimonifxyy")
-        await new_client.join_chat("benefitubot")
-        await new_client.join_chat("fxyypublicvl")
+        await new_client.join_chat("dhurove")
+        await new_client.join_chat("publicrico")
     except UserAlreadyParticipant:
         pass
 
